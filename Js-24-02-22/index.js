de1f9ed33f51217aa06ac60597034da6569875fb
Sanjay_Kumar <59865828+sanjayhns102@users.noreply.github.com>
2022-02-24 19:32:41 +0530

    Add files via upload

---
+++ Js-24-02-22/index.js
@@ -0,0 +1,33 @@
+// const arr=[5,7,8,6,7,1,3,4];
+// const arr2=arr.map((item)=>item*2)
+// console.log(arr2);
+
+
+// const arr=[{name:"Sanjay",age:24,email:"sanjay@dsx.in"},
+//             {name:"Raj",age:15,email:"raj@dsx.in"},
+//             {name:"Narender",age:20,email:"narender@dsx.in"},
+//             {name:"Kapil",age:26,email:"kp@dsx.in"},
+//             {name:"Amit",age:22,email:"amit@dsx.in"},
+//             {name:"Harry",age:12,email:"harry@dsx.in"},
+//             {name:"Mohit",age:29,email:"mohit@dsx.in"}          
+//             ];
+// const arr2= arr.filter((person)=>{
+//    return person.age>=18;
+// });
+// console.log(arr2);
+
+
+
+
+// const arr=[{name:"Sanjay",age:24,email:"sanjay@dsx.in",donateAmount:500},
+//             {name:"Raj",age:15,email:"raj@dsx.in",donateAmount:400},
+//             {name:"Narender",age:20,email:"narender@dsx.in",donateAmount:500},
+//             {name:"Kapil",age:26,email:"kp@dsx.in",donateAmount:100},
+//             {name:"Amit",age:22,email:"amit@dsx.in",donateAmount:100},
+//             {name:"Harry",age:12,email:"harry@dsx.in",donateAmount:200},
+//             {name:"Mohit",age:29,email:"mohit@dsx.in",donateAmount:100}          
+//             ];
+
+
+// let totalDonation = arr.reduce((total,person)=> total+person.donateAmount,0);
+// console.log(totalDonation);
